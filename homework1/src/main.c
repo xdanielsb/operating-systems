c1fb86449c9b22dbaa96d27313d8cb3a99827680
Daniel <dfsantosb@correo.udistrital.edu.co>
2017-02-08 09:08:31 -0500

    Add program in c.

---
+++ homework1/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("int :%d \n", sizeof(int));
+    printf("int :%d \n", sizeof(double));
+    printf("int :%d \n", sizeof(char));
+    printf("int :%d \n", sizeof(long));
+    printf("int :%d \n", sizeof(unsigned long));
+    return 0;
+
+}
